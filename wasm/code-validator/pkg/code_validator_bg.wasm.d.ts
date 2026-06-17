3fb28c560b06196edbe7e386daa63241992ae674
garyleelty <tl3285@nyu.edu>
2026-06-18 00:23:24 +0800

    feat(wasm): WASM build output (646KB)

---
+++ wasm/code-validator/pkg/code_validator_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_interpresult_free: (a: number, b: number) => void;
+export const interpresult_errors: (a: number) => [number, number];
+export const interpresult_output: (a: number) => [number, number];
+export const interpresult_success: (a: number) => number;
+export const interpresult_warnings: (a: number) => [number, number];
+export const run_code: (a: number, b: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
